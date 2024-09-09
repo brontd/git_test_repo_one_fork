582ae42477b07af2b7e8c3df4388fcefa90e6f3c
bronttest <165678450+bronttest@users.noreply.github.com>
2024-09-09 17:25:30 -0400

    Update main.py
    newer

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def testing():
     print ('DUNE: A beginning is a very delicate time... 10,131. ')
     print ('the movie was pretty good too')
 
-def new_function():
-    print ('all the newness')
+def newer_function():
+    print ('all the newness changed to newer')
 
 if __name__ == '__main__':
     testing()
