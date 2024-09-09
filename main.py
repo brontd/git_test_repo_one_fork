97b31b1829cbcd4c1c26b416b55aad4e155b98bd
bronttest <165678450+bronttest@users.noreply.github.com>
2024-09-09 17:11:06 -0400

    Update main.py
    updated main

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 def testing():
     print ('DUNE: A beginning is a very delicate time... 10,131. ')
     print ('the movie was pretty good too')
-  
+
+def new_function():
+    print ('all the newness')
+
 if __name__ == '__main__':
     testing()
