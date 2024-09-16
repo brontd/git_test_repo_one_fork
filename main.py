d1398a80007d97fc312c246b035720bbd7362d32
bront <bront.davis@uky.edu>
2024-09-16 10:42:03 -0400

    add yet_another_test_function()

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-# main.py - 13, 
+# main.py --> 20
+
 def testing():
     print ('DUNE: A beginning is a very delicate time... 10,131. ')
     print ('the movie was pretty good too')
 
 def test_function():
+    print ('test function... should be in all branches')
+
+def yet_another_test_function():
     print ('test function... should be in all branches')
 
 if __name__ == '__main__':
